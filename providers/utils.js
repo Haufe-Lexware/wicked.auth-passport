e9e3d544bc082b9d477a71423f475bc01c46c035
Martin Danielsson <martin@danielsson.de>
2017-02-14 14:02:17 +0100

    Use wicked SDK for monitoring

--- providers/utils.js
+++ providers/utils.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const debug = require('debug')('auth-passport:utils');
 const wicked = require('wicked-sdk');
 const url = require('url');
+const fs = require('fs');
+const path= require('path');
 
 const utils = function () { };
 
@@ -217,5 +219,25 @@ utils.cors = function () {
     };
     return cors(optionsDelegate);
 };
+
+utils._packageVersion = null;
+utils.getVersion = function () {
+    if (!utils._packageVersion) {
+        const packageFile = path.join(__dirname, '..', 'package.json');
+        if (fs.existsSync(packageFile)) {
+            try {
+                const packageInfo = JSON.parse(fs.readFileSync(packageFile, 'utf8'));
+                if (packageInfo.version)
+                    utils._packageVersion = packageInfo.version;
+            } catch (ex) {
+                console.error(ex);
+            }
+        }
+        if (!utils._packageVersion) // something went wrong
+            utils._packageVersion = "0.0.0";
+    }
+    return utils._packageVersion;
+};
+
 
 module.exports = utils;
